dffd1057725fa085418dcaaa4d8928e799427878
Heiner Salazar Navarro <hsalazar@mpz.go.cr>
2026-02-17 15:42:21 -0600

    mensaje corto del cambio

--- app/dashboard/counter/page.tsx
+++ app/dashboard/counter/page.tsx
@@ -1,14 +1,30 @@
-import { Span } from "next/dist/trace";
+'use client';
+import { useState } from "react";
+
+
+export const metadata = {
+ title: 'Counter Page',
+ description: 'Counter',
+};
 
 export default function NamePage() {
+
+  const [count, setCount] = useState(5)
+
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full">
+    <div className="flex flex-col items-center justify-center w-full min-h-screen">
       <span>Productos en el carrito</span>
-      <span className="text-9xl">10</span>
+      <span className="text-9xl">{count}</span>
 
       <div className="flex">
-        <button className="">
-          +1
+        <button onClick={() => setCount(count + 1)} 
+          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-100px mr-2 w-25">
+            +1
+        </button>
+
+        <button onClick={() => setCount( count - 1)} 
+            className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-100px mr-2 w-25">
+            -1
         </button>
       </div>
     </div>
